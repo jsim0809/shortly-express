1894593f104f6a44b476f5208f34fbd795c9d406
Jeremy Sim <jsim0809@gmail.com>
2020-02-28 11:32:33 -0800

    Working on cookies.

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,23 @@
 const parseCookies = (req, res, next) => {
+
+  if (!req.headers.Cookie) {
+    // req.cookies =
+  }
+
+
 };
+
+const findShortlyId = (string) => {
+  let startIndex = 0;
+  let keyStart = string.indexOf('=');
+
+  if (keyStart === -1) {
+    return null;
+  } else {
+    string.slice(keyStart, string.indexOf(';'));
+  }
+
+};
+
 
 module.exports = parseCookies;
